7fdbe2d0c5ec15de9d22fab3856392e91ca1aa18
Hamza Jadid <haljadid@proximie.com>
2022-05-31 08:51:38 +0300

    added ios glue project

--- glue_core/src/lib.rs
+++ glue_core/src/lib.rs
@@ -1,4 +1,4 @@
-fn gcd(mut n: u64, mut m: u64) -> u64 {
+pub fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
     while m != 0 {
         if m < n {
---
+++ glue_ios/src/main.rs
@@ -0,0 +1,6 @@
+use glue_core::gcd;
+
+fn main() {
+    gcd(24, 6);
+    println!("gcd(24, 6) = {}", gcd(24, 6));
+}
