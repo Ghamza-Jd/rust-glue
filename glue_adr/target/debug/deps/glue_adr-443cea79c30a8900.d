7f3aeaab7f9a7f170818cfc774530e63a309adac
Hamza Jadid <haljadid@proximie.com>
2022-06-03 11:17:26 +0300

    added android glue

---
+++ glue_adr/target/debug/deps/glue_adr-443cea79c30a8900.d
@@ -0,0 +1,5 @@
+/Users/hamzajadid/Workspace/rust-glue/glue_adr/target/debug/deps/glue_adr-443cea79c30a8900.rmeta: src/lib.rs
+
+/Users/hamzajadid/Workspace/rust-glue/glue_adr/target/debug/deps/glue_adr-443cea79c30a8900.d: src/lib.rs
+
+src/lib.rs:
---
+++ glue_adr/target/debug/deps/glue_adr-b7ef4d9261852ce5.d
@@ -0,0 +1,5 @@
+/Users/hamzajadid/Workspace/rust-glue/glue_adr/target/debug/deps/glue_adr-b7ef4d9261852ce5.rmeta: src/lib.rs
+
+/Users/hamzajadid/Workspace/rust-glue/glue_adr/target/debug/deps/glue_adr-b7ef4d9261852ce5.d: src/lib.rs
+
+src/lib.rs:
