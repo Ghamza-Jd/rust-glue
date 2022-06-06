73a19895f430f3ffbe07ff80384a761163c5ea9a
Ghamza-Jd <haljadid@proximie.com>
2022-06-07 00:49:47 +0300

    feat: added wasm main

--- glue_wasm/src/lib.rs
+++ glue_wasm/src/lib.rs
@@ -1,8 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use wasm_bindgen::prelude::*;
+use glue_core::gcd;
+
+#[wasm_bindgen]
+pub fn rust_gcd(n: u64, m: u64) -> u64 {
+    return gcd(n, m);
 }
