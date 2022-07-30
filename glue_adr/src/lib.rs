216d00f8707940ff311bf250b74dadeb3c17e571
Hamza Jadid <haljadid@proximie.com>
2022-07-30 13:15:23 +0300

    android support

--- glue_adr/src/lib.rs
+++ glue_adr/src/lib.rs
@@ -1,8 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use glue_core::gcd;
+
+struct AndroidGlue {}
+
+impl AndroidGlue {
+    pub fn android_gcd(n: u64, m: u64) -> u64 {
+        gcd(n, m)
     }
 }
